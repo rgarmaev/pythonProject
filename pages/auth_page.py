fdc64abe82b3f2b15cc959587136ac5a74bab6fe
R.Garmaev <garmaev.rinchin@gmail.com>
2024-04-19 17:13:54 +0300

    test_selenium_petfriends_pytest_cookie.py update

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,6 +1,6 @@
 from pages.base import WebPage
 from pages.elements import WebElement
-
+import pickle
 
 class AuthPage(WebPage):
 
@@ -9,7 +9,19 @@ class AuthPage(WebPage):
         super().__init__(web_driver, url)
 
     email = WebElement(id='email')
-
     password = WebElement(id='pass')
-
     btn = WebElement(class_name='btn.btn-success')
+
+    def login(self, user_email, user_password):
+        self.email.send_keys(user_email)
+        self.password.send_keys(user_password)
+        self.btn.click()
+
+        # После успешной аутентификации сохраняем cookies в файл
+        cookies = self.web_driver.get_cookies()
+        with open('my_cookies.txt', 'wb') as cookies_file:
+            pickle.dump(cookies, cookies_file)
+    
+    
+
+
--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,14 +1,24 @@
-import pytest
+import pickle
 from pages.auth_page import AuthPage
 
-def test_authorisation(web_browser):
 
+def test_authorisation(web_browser):
     page = AuthPage(web_browser)
 
     page.email.send_keys('rgarmaev@mail.ru')
-
     page.password.send_keys("xy6nY36uq6")
-
     page.btn.click()
 
+    # Получаем cookies из текущего веб-драйвера страницы
+    cookies = web_browser.get_cookies()
+
+    # Сохраняем cookies в файл с помощью pickle
+    with open('my_cookies.txt', 'wb') as cookies_file:
+        pickle.dump(cookies, cookies_file)
+
+    # Проверяем текущий URL после аутентификации
     assert page.get_current_url() == 'https://petfriends.skillfactory.ru/all_pets'
+
+
+
+
